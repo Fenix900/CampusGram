55a0f2cb3b1a4ed677990d718940c514ac543592
Fenix9000 <93600617+Fenix9000@users.noreply.github.com>
2024-04-16 16:38:39 +0200

    The login page is done

--- project-app/src/Components/Login_Signup/Login_Signup.jsx
+++ project-app/src/Components/Login_Signup/Login_Signup.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Login_Signup.css"
 import login_button_icon from "../Assets/login.png"
 import password_icon from "../Assets/Password.png"
@@ -6,27 +6,34 @@ import create_user_icon from "../Assets/Signup.png"
 import username_icon from "../Assets/username.png"
 
 export const Login_Signup = () => {
+  {/*Maybe add forget password here*/}
+
+  const [action, setAction] = useState("Login");
+
   return (
+    /*This part is the whole sign in page*/
     <div className='container'>
     <div className='header'>
-      <div className='headerText'>Sign up</div>
+      <div className='headerText'>{action}</div>
     </div>
-
-    <div className='userInputs'>
+    {/*user input field: Username and password*/}
+    <div className='userInputs'> 
       <div className='input'>
-        <img src={username_icon} width={15} alt=''/>
-        <input type='text'/>
+        <img src={username_icon} width={20} alt=''/>
+        <input type='text' placeholder='Name'/>
       </div>
       <div className='input'>
-        <img src={password_icon} width={15} alt=''/>
-        <input type='password'/>
+        <img src={password_icon} width={20} alt=''/>
+        <input type='password' placeholder='Password'/>
       </div>
+      {/*Two buttons for either log in or create a new user*/}
       <div className="actionButtons">
-        <div className="submitt">Create new user</div>
-        <div className="submitt">Login</div>
+        <div className={action==="Login"?"submitt grayOut":"submitt"} onClick={()=>{setAction("New user")}}>New user</div>
+        <div className={action==="New user"?"submitt grayOut":"submitt"} onClick={()=>{setAction("Login")}}>Login</div>
       </div>
     </div>
 
   </div>
+
   )
 }
