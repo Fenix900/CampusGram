9d77eda2f413f41fe332e643baea742b58adc7d1
Fenix9000 <93600617+Fenix9000@users.noreply.github.com>
2024-05-06 15:50:17 +0200

    Added some suggested user placeholder

--- project-app/src/Components/Home/Home.jsx
+++ project-app/src/Components/Home/Home.jsx
@@ -1,23 +1,24 @@
 import { Box, Container, Flex } from '@chakra-ui/react'
 import React from 'react'
 import { FeedPosts } from '../Posts_Feed/FeedPosts'
+import { Suggestions } from '../suggestionForUsers/Suggestions'
 
 //The home page, the side bar should be here, the feed and also a suggestion, but is only shown during bigger screens
 export default function Home() {
   return (
       <Container maxW={"container.lg "}>
-        <Flex gap={20}>
-          <Box flex={2} py={100}>
+        <Flex gap={7}>
+          <Box flex={2} py={50}>
             <FeedPosts/>
           </Box>
           <Box 
           flex={3} 
-          mr={20}
+          mr={5}
           display={{base:"none", lg:"block"}}
           maxW={"300px"}
-          border={"1px solid red"}
+          borderLeft={"1px solid grey"}
           >
-            suggested users here
+            <Suggestions></Suggestions>
           </Box>
         </Flex>
       </Container>
