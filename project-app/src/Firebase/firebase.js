0503bb5a9f3b0dd63ef67ee537fd48adbd8873b8
Fenix9000 <93600617+Fenix9000@users.noreply.github.com>
2024-05-07 14:41:24 +0200

    Removed comments

--- project-app/src/Firebase/firebase.js
+++ project-app/src/Firebase/firebase.js
@@ -1,14 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getAuth} from "firebase/auth";
-import {getFirestore} from "firebase/firestore";
-import {getStorage} from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {getAuth} from "firebase/auth"; //For authentication
+import {getFirestore} from "firebase/firestore"; //For keeping track of users, information about users and their posts
+import {getStorage} from "firebase/storage"; //For storage of images
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAZzyhJ0XpSvya47k2IbrUHICQm1OLvsiM",
   authDomain: "campusgram-5877d.firebaseapp.com",
