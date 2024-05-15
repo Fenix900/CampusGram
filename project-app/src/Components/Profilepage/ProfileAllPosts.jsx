f9daf8c9f9656389a4c12be93c94b33575b4ca25
Fenix9000 <93600617+Fenix9000@users.noreply.github.com>
2024-05-15 14:35:20 +0200

    Added a single post
    Now we could see each post as a component, these component has a overlay where we will be able to see number of likes and comments. Also added a grid for all the posts

--- project-app/src/Components/Profilepage/ProfileAllPosts.jsx
+++ project-app/src/Components/Profilepage/ProfileAllPosts.jsx
@@ -1,7 +1,19 @@
+import { Grid, GridItem, Image } from '@chakra-ui/react'
 import React from 'react'
+import ProfileImg from "../Assets/TestProfile.png"
+import { ProfileSinglePost } from './ProfileSinglePost'
+
 
 export const ProfileAllPosts = () => {
   return (
-    <div>ProfileAllPosts</div>
+    <div>
+      <Grid templateColumns='repeat(3, 1fr)' gap={4}>
+        <ProfileSinglePost img={ProfileImg}/>
+        <ProfileSinglePost img={ProfileImg}/>
+        <ProfileSinglePost img={ProfileImg}/>
+        <ProfileSinglePost img={ProfileImg}/>
+        <ProfileSinglePost img={ProfileImg}/>
+      </Grid>
+    </div>
   )
 }
--- project-app/src/Components/Profilepage/ProfileSinglePost.jsx
+++ project-app/src/Components/Profilepage/ProfileSinglePost.jsx
@@ -1,7 +1,33 @@
+import { Flex, GridItem, Image, Text } from '@chakra-ui/react'
 import React from 'react'
 
-export const ProfileSinglePost = () => {
+export const ProfileSinglePost = ({img}) => {
   return (
-    <div>ProfileSinglePost</div>
+    <div>
+        <GridItem cursor={"pointer"} borderRadius={6} aspectRatio={1/1} position={"relative"}>
+            <Flex 
+            flexDirection={{base:"column",md:"row"}}
+            opacity={0} 
+            _hover={{opacity:0.7}} 
+            bg={"black"} 
+            transition={"all 0.7 ease"} 
+            justifyContent={"center"} 
+            h={"full"} 
+            w={"full"} 
+            position={"absolute"}
+            gap={{base:0,sm:3, md:6}}
+            alignItems={"center"}>
+                <Flex flexDirection="column" textAlign="center" fontSize={{sm:"md", base:"xs", lg:"xl"}}>
+                    <Text fontWeight={"700"}>Likes</Text> 
+                    20
+                </Flex>
+                <Flex flexDirection="column" textAlign="center" fontSize={{sm:"md", base:"xs", lg:"xl"}}>
+                    <Text fontWeight={"700"}>comments</Text>    {/*Replace this and likes with icons */}
+                    5
+                </Flex>
+            </Flex> 
+            <Image src={img} h={"100%"} w={"100%"} objectFit={"cover"}/>
+        </GridItem>
+    </div>
   )
 }
