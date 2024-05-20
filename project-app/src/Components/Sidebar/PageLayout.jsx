c41856637f4d628fe20306f2e485003d48bf7940
Fenix9000 <93600617+Fenix9000@users.noreply.github.com>
2024-05-20 17:10:20 +0200

    Some changes with non registered users
    I changed so that users that isn't logged in can see other users accounts. Also added a bar at the top to go to the register page.
    (This might be removed. But it might be fun to see other profiles without signing in.)

--- project-app/src/Components/Sidebar/PageLayout.jsx
+++ project-app/src/Components/Sidebar/PageLayout.jsx
@@ -1,23 +1,36 @@
-import { Box, Flex } from '@chakra-ui/react'
+import { Box, Button, Flex, VStack } from '@chakra-ui/react'
 import React from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation } from 'react-router-dom';
 import Sidebar from './Sidebar';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../../Firebase/firebase';
+import SignupNavBar from '../TopNavBar/SignupNavBar';
 
 const PageLayout = ({children}) => {
-     const {pathname} = useLocation();
+     const locationPath = useLocation();
+     console.log(locationPath);
+     const [user, loading, error] = useAuthState(auth);
+     //This showsSidebar shows the sidabar if the location isn't the login page and if there is a signed in user
+     const navSigninOrSignup = locationPath.pathname !=="/" && !user;
+     const showSidebar = locationPath.pathname !== "/" && user;
+
+
   return (
-    <Flex>
-        {/*This side is for the side bar (LEFT) */}
-        {pathname !== '/' ? (        
-        <Box w={{base:"75px", md:"230px"}}> 
-            <Sidebar/>
-        </Box>
-        ): null}
-        {/*This side is for the page (RIGHT) */}
-        <Box  flex={1} w={{base:"calc(100%-70px)", md:"calc(100%-200px)"}}>
-            {children}
-        </Box>
-    </Flex>
+    <VStack>
+        {navSigninOrSignup ? <SignupNavBar /> : null}
+        <Flex>
+            {/*This side is for the side bar (LEFT) */}
+            {showSidebar ? (        
+            <Box w={{base:"75px", md:"230px"}}> 
+                <Sidebar/>
+            </Box>
+            ): null}
+            {/*This side is for the page (RIGHT) */}
+            <Box  flex={1} w={{base:"calc(100%-70px)", md:"calc(100%-200px)"}}>
+                {children}
+            </Box>
+        </Flex>
+    </VStack>
     )
 }
 
