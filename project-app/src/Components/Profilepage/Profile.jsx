d8fa84471bfaf32272bd67bae6b1fcae5818aa68
Fenix9000 <93600617+Fenix9000@users.noreply.github.com>
2024-05-23 11:28:02 +0200

    Display user information
    Have now added the user information into the profile. Also added a placeholder for when the profile icon is not set yet.

--- project-app/src/Components/Profilepage/Profile.jsx
+++ project-app/src/Components/Profilepage/Profile.jsx
@@ -1,4 +1,4 @@
-import { Box, Container, Flex } from '@chakra-ui/react'
+import { Box, Container, Flex, Spinner } from '@chakra-ui/react'
 import React from 'react'
 import { ProfileAllPosts } from './ProfileAllPosts'
 import { ProfileInformation } from './ProfileInformation'
@@ -11,17 +11,15 @@ export const Profile = () => {
   const { username } = useParams(); //Since we use "username" as a parameter in the url we can extract it
   const usernameLower = username.toLowerCase(); //make it to lower for safer query searching
   const {isLoading,userProfileInfo} = useGetProfileByName(usernameLower)
-  console.log(userProfileInfo)
   if(!userProfileInfo && !isLoading){ //no user found
-    console.log("user not found")
     return <CouldNotFindUser />
   }
-
   return (
     <div>
         <Container maxW={"container.lg"}>
             <Flex px={5} pt={10} w={"full"} mx={"auto"} flexDirection={"column"} pb={{base:2, md:5}}>
-                <ProfileInformation/>
+                {!isLoading && userProfileInfo ? <ProfileInformation/> 
+                : <Spinner size={"xl"}/> /*When the user is being loaded we show a spinner */} 
             </Flex>
             <Flex px={{base:2, md:4}} maxW={"full"} mx={"auto"} direction={"column"}>
                 <ProfileSections />
--- project-app/src/Components/Profilepage/ProfileInformation.jsx
+++ project-app/src/Components/Profilepage/ProfileInformation.jsx
@@ -2,9 +2,13 @@ import { Avatar, Button, Flex, Text, VStack, useDisclosure } from '@chakra-ui/re
 import React from 'react'
 import ProfileImg from "../Assets/TestProfile.png"
 import EditProfileInfo from './EditProfileInfo'
+import useGetProfileByName from '../../hooks/useGetProfileByName'
+import useProfileInfoStore from '../../globalStates/profileInfoStore'
+import placeholder from '../Assets/NoImage.png'
 
 export const ProfileInformation = () => {
     const { isOpen, onOpen, onClose } = useDisclosure()
+    const {userProfileInfo} = useProfileInfoStore() //Gets the information about the user (from profileInfoStore.js)
 
   return (
     //This is the header for the profile page, here we will see the profile image,
@@ -12,10 +16,10 @@ export const ProfileInformation = () => {
     <div>
         {isOpen ? (<EditProfileInfo isOpen={isOpen} onClose={onClose}/>) : null} {/*This have to be better centered so phones have it in the middle */}
         <Flex py={7} direction={{base:"column", sm:"row"}} gap={2}>
-            <Avatar size={{base:"xl", md:"2xl"}} name='Replace Name' src={ProfileImg} alt="N/A" justifySelf={"flex-start"} alignSelf={"flex-start"} mx={{base:2, md:10}}/>
+            <Avatar size={{base:"xl", md:"2xl"}} name={userProfileInfo.username} src={userProfileInfo.profilePicture  ? userProfileInfo.profilePicture : placeholder} justifySelf={"flex-start"} alignSelf={"flex-start"} mx={{base:2, md:10}}/>
             <VStack gap={{base:2, md:5}} alignItems={"start"}>
                 <Flex direction={{base:"column", sm:"row"}} justifyContent={{base:"center", sm:"flex-start"}} w={"full"}>
-                    <Text fontSize={{base:"xl", md:"2xl"}} fontWeight={"700"}>The Username</Text>
+                    <Text fontSize={{base:"xl", md:"2xl"}} fontWeight={"700"}>{userProfileInfo.username}</Text>
                      <Flex justifyContent={{md:"center", base:"left"}} alignItems={"center"} ml={{sm:10, base:0}}>
                         <Button bg={"blue.400"} color={"black"} size={{base:"xs", md:"sm"}} _hover={{bg:"blue.200"}} onClick={onOpen}>
                             Edit Profile
@@ -24,20 +28,20 @@ export const ProfileInformation = () => {
                 </Flex>
                 <Flex alignItems={"center"} gap={10}>
                     <VStack>
-                        <Text fontSize={"lg"} fontWeight={"800"}>10</Text>
+                        <Text fontSize={"lg"} fontWeight={"800"}>{userProfileInfo.posts.length}</Text>
                         <Text fontSize={"sm"} textAlign="center">Posts</Text>
                     </VStack>
                     <VStack>
-                        <Text fontSize={"lg"} fontWeight={"800"}>10</Text>
+                        <Text fontSize={"lg"} fontWeight={"800"}>{userProfileInfo.followers.length}</Text>
                         <Text fontSize={"sm"} textAlign="center">Followers</Text>
                     </VStack>
                     <VStack>
-                        <Text fontSize={"lg"} fontWeight={"800"}>10</Text>
+                        <Text fontSize={"lg"} fontWeight={"800"}>{userProfileInfo.following.length}</Text>
                         <Text fontSize={"sm"} textAlign="center">Following</Text>
                     </VStack>
                 </Flex>
-                <Text fontWeight={"700"} textDecor={"underline"} fontSize={"md"}>Fenix900 (what the user wants?)</Text>
-                <Text fontSize={"sm"} mt={{base:0, md:-2}}>The discription for what I am doing and what my lift is!!!</Text>
+                <Text fontWeight={"700"} textDecor={"underline"} fontSize={"md"}>{userProfileInfo.profileDisplayName}</Text>
+                <Text fontSize={"sm"} mt={{base:0, md:-2}}>{userProfileInfo.bio}</Text>
             </VStack>
         </Flex> 
     </div>
