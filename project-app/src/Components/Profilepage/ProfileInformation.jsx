6863d0d1876deb72c2520b21bbeefcf7bdfeff0d
Fenix9000 <93600617+Fenix9000@users.noreply.github.com>
2024-05-24 14:45:54 +0200

    We can now add and remove users from firebase
    The following array on firebase can now be updated and shows people we follow. But we cannot see followers yet. When this is merged with main we will get the profile information from the different places with hook that was implemented there so we can update both users at the same time

--- project-app/src/Components/Profilepage/ProfileInformation.jsx
+++ project-app/src/Components/Profilepage/ProfileInformation.jsx
@@ -1,8 +1,10 @@
 import { Avatar, Button, Flex, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
 import ProfileImg from "../Assets/TestProfile.png"
+import useFollowAndUnfollowUser from '../../hooks/useFollowAndUnfollowUser'
 
 export const ProfileInformation = () => {
+    const {isFollowing, handleFollowOrUnfollowUserf} = useFollowAndUnfollowUser("MwnTpfllRwNmHzoF7QeC5vTdZ4e2");
   return (
     //This is the header for the profile page, here we will see the profile image,
     //a description and following/followers/posts
@@ -13,9 +15,10 @@ export const ProfileInformation = () => {
                 <Flex direction={{base:"column", sm:"row"}} justifyContent={{base:"center", sm:"flex-start"}} w={"full"}>
                     <Text fontSize={{base:"xl", md:"2xl"}} fontWeight={"700"}>The Username</Text>
                      <Flex justifyContent={{md:"center", base:"left"}} alignItems={"center"} ml={{sm:10, base:0}}>
-                        <Button bg={"blue.400"} color={"black"} size={{base:"xs", md:"sm"}} _hover={{bg:"blue.200"}}>
-                            Edit Profile
+                        <Button bg={"blue.400"} color={"black"} size={{base:"xs", md:"sm"}} _hover={{bg:"blue.200"}} onClick={handleFollowOrUnfollowUserf}>
+                            Follow
                         </Button>
+                        <Button bg={isFollowing ? "green" : "red"}></Button>
                      </Flex>
                 </Flex>
                 <Flex alignItems={"center"} gap={10}>
