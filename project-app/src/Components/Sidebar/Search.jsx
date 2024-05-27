dc536a80cc72d8684ec1dee94ef409179989fb04
Fenix9000 <93600617+Fenix9000@users.noreply.github.com>
2024-05-27 20:15:41 +0200

    We can now fetch users by searching
    I have implmented a modal that pops up with a search bar that lets the user write a name, although we can only get results if we write in the correct username. (Maybe work on it more but for now it is enough for me). To get the result when we write in a search on the modal we use the custom hook which searches for the user. Next step is to display the user result

--- project-app/src/Components/Sidebar/Search.jsx
+++ project-app/src/Components/Sidebar/Search.jsx
@@ -1,39 +1,77 @@
-import { Box, Flex, Image, Tooltip } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Button, Flex, FormControl, FormHelperText, FormLabel, Image, Input, InputGroup, InputRightAddon, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text, Tooltip, useDisclosure } from '@chakra-ui/react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import {Link as ReactRouterLink} from 'react-router-dom'
 import searchIcon from "../Assets/search.png"
+import useSearchForUser from '../../hooks/useSearchForUser'
 
 const Search = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [username, setUsername] = useState('');
+  const {isLoading, user, searchUser} = useSearchForUser();
+  const handleClose = () => {
+    onClose()
+    setUsername('')
+  }
+  const handleSearch = (e) => {
+    e.preventDefault();
+    searchUser(username);
+  }
+  console.log(user)
+
   return (
-    <Tooltip hasArrow label={"Search"} placement='right' openDelay={300} ml={2} display={{md:"none", base:"block" }}>
-        <Link 
-        display={"flex"}
-        to={"/"}
-        as={ReactRouterLink}
-        alignItems={"center"}
-        gap={5}
-        _hover={{bg:"gray.700"}}
-        borderRadius={6}
-        p={2}
-        w={"full"}
-        justifyContent={{base:"center", md:"flex-start"}}
-        >
-          <Flex
-          alignItems={"center"}
-          gap={5}
-          _hover={{bg:"gray.700"}}
-          borderRadius={6}
-          p={2}
-          w={"full"}
-          justifyContent={{base:"center", md:"flex-start"}}
-          mt={"auto"}
-          >
-            <Image src={searchIcon} width={'50px'} />
-            <Box display={{base:"none", md:"block"}}>Search</Box>
-          </Flex>
-        </Link> 
-    </Tooltip> 
+    <>
+      <Tooltip hasArrow label={"Search"} placement='right' openDelay={300} ml={2} display={{md:"none", base:"block" }}>
+            <Flex
+            alignItems={"center"}
+            gap={5}
+            _hover={{bg:"gray.700"}}
+            borderRadius={6}
+            p={2}
+            w={"full"}
+            justifyContent={{base:"center", md:"flex-start"}}
+            mt={"auto"}
+            onClick={onOpen}
+            >
+              <Image src={searchIcon} width={'50px'} />
+              <Box display={{base:"none", md:"block"}}>Search</Box>
+            </Flex>
+      </Tooltip> 
+      {/*Modal overlay that shows up when we want to search for a user */}
+      <Modal isOpen={isOpen} onClose={handleClose}>
+        <ModalOverlay />
+        <ModalContent h={"70%"} maxH={"90%"}>
+          <ModalCloseButton />
+          <ModalBody pt={"50px"}>
+            <form>
+            <FormControl>
+              <FormLabel fontSize={"lg"}>Search by username</FormLabel>
+              <InputGroup size='sm'>
+                <Input
+                  type='text'
+                  placeholder='Username'
+                  _placeholder={{ color: "gray" }}
+                  value={username} // Bind state to input value
+                  onChange={(e) => {setUsername(e.target.value)}} //Saves the change in username
+                />
+                {/* This is the button to the left of the search field */}
+                <InputRightAddon bg={"transparent"} border={"none"}>
+                  <Button w={"full"} h={"full"} isLoading={isLoading} onClick={handleSearch}>                  
+                    Search
+                  </Button>
+                </InputRightAddon>
+              </InputGroup>
+            </FormControl>
+            </form>
+          </ModalBody>
+          <ModalFooter justifyContent={"center"} pt={"30px"}>
+            <Button colorScheme='blue' onClick={handleClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   )
 }
 
---
+++ project-app/src/hooks/useSearchForUser.js
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import { firestore } from '../Firebase/firebase';
+import { collection, query, where, getDocs } from 'firebase/firestore'
+import { useDisplayError } from './useDisplayError';
+
+
+const useSearchForUser = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [user, setUser] = useState(null);
+    const showMessage = useDisplayError();
+
+    const searchUser = async (username) => { //Username is the search string we want to look for in our documents
+        setIsLoading(true)
+        try {
+          const usersRef = collection(firestore, 'users')
+          const q = query(usersRef, where('usernameLower', '==', username.toLowerCase()))
+          const querySnapshot = await getDocs(q) //all the documents where the "usernameLower" matches the input "username"
+          
+          if (querySnapshot.empty) {
+            setUser(null)
+            showMessage("No users found","Could not find user with name "+username, "warning")
+          } else {
+            querySnapshot.forEach((doc) => {
+              setUser({ id: doc.id, ...doc.data() })
+            })
+          }
+        } catch (e) {
+          console.log(e)
+          showMessage("Error",e,"error")
+        } finally {
+          setIsLoading(false)
+        }
+      }
+    
+      return { isLoading, user, searchUser}
+}
+
+export default useSearchForUser
