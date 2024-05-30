38ae5c4943a22e556508f268dcf27cc569f5e16b
Fenix9000 <93600617+Fenix9000@users.noreply.github.com>
2024-05-30 15:06:08 +0200

    Added custom hook to fetch user by userID
    Now we can fetch the users by their userID which we didn't have, we can now use the postedByID to fetch the user info for like the username or profile picture

---
+++ project-app/src/hooks/useFetchUserInfoByUserID.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+import { firestore } from '../../Firebase/firebase';
+import { doc, getDoc } from 'firebase/firestore';
+
+const useFetchUserInfo = (userID) => {
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => { //Function to fetch user profile by using userID from the inputs
+    const fetchuser = async () => {
+      setIsLoading(true);
+      setUser(null)
+      try {
+        const userDocRef = doc(firestore, 'users', userId); //Ref to users doc
+        const userDoc = await getDoc(userDocRef); //Fetch the users document
+        if (userDoc.exists()) {
+            setUser(userDoc.data())
+        } else {
+            console.log(`No user found with ID: ${userId}`);
+        }
+      } catch (error) {
+        console.log("COULD NOT GET USER BY ID WITH ERROR:", error)
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    if (userID.length > 0) {
+      fetchuser();
+    }
+  }, [userID]);
+
+  return { user, isLoading};
+};
+
+export default useFetchUserInfo;
