53e769a9be9542d6cce13cf51c1323fd8198ca4f
Fenix9000 <93600617+Fenix9000@users.noreply.github.com>
2024-05-31 14:31:20 +0200

    Fetching users posts
    We can now fetch the users posts depending on if we follow them or not.

--- project-app/src/Components/Posts_Feed/FeedPosts.jsx
+++ project-app/src/Components/Posts_Feed/FeedPosts.jsx
@@ -3,13 +3,16 @@ import { OnePost } from './OnePost'
 import TestPost1 from "../Assets/LIS_Test.png"
 import TestPost2 from "../Assets/mc.png"
 import profPIC from "../Assets/username.png"
+import useGetPosts from '../../hooks/useGetPosts'
 
 //The overall posts, this will show all the posts with each post as <OnePost>
 
 export const FeedPosts = () => {
+  const {posts, isLoading} = useGetPosts();
+  console.log(posts)
   return (
     <Container maxW={"continer.sm"} py={10} px={2}>
-        <Text>POST WILL BE HERE SOON</Text>
+
     </Container>
   )
 }
