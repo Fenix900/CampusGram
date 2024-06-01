2b5a3b68eda2a469e472206615d6f77d13be8835
Fenix9000 <93600617+Fenix9000@users.noreply.github.com>
2024-06-01 14:17:28 +0200

    added set user profile to profile link and error fixes
    Now when we press the link to our profile it sets the user profile information, but we also have fixed an small error with the icons where we tried to use some customizations directly on the link but now we have a box instead over the link. No more errors now from that

--- project-app/src/Components/Sidebar/HomeSidebar.jsx
+++ project-app/src/Components/Sidebar/HomeSidebar.jsx
@@ -7,17 +7,15 @@ import homeIcon from "../Assets/home.png"
 const HomeSidebar = () => {
   return (
     <Tooltip hasArrow  label={"Home"} placement='right' openDelay={300} ml={2} display={{md:"none", base:"block" }}>
+      <Box alignItems={"center"} justifyContent={{base:"center", md:"flex-start"}} borderRadius={6}>
         <Link 
         display={"flex"}
         to={"/home"}
         as={ReactRouterLink}
-        alignItems={"center"}
         gap={5}
         _hover={{bg:"gray.700"}}
-        borderRadius={6}
         p={2}
         w={"full"}
-        justifyContent={{base:"center", md:"flex-start"}}
         >
             <Flex
             alignItems={"center"}
@@ -33,6 +31,7 @@ const HomeSidebar = () => {
                 <Box display={{base:"none", md:"block"}}>Home</Box>
             </Flex>
         </Link> 
+      </Box>
     </Tooltip> 
   )
 }
