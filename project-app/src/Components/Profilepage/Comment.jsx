d04edbe84b655b18452d8f3f6873945a4fe17f88
Fenix9000 <93600617+Fenix9000@users.noreply.github.com>
2024-06-04 14:46:46 +0200

    Small fix
    Fixed a small problem where the navigation was throwing the user between two profiles for some reason. Did copy the implementation from suggestedUser component which didn't do that

--- project-app/src/Components/Profilepage/Comment.jsx
+++ project-app/src/Components/Profilepage/Comment.jsx
@@ -2,9 +2,11 @@ import { Avatar, Box, Flex, Spinner, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
 import useFetchUserInfoByUserID from '../../hooks/useFetchUserInfoByUserID'
 import { Link } from 'react-router-dom'
+import useProfileInfoStore from '../../globalStates/profileInfoStore'
 
 const Comment = ({comment}) => {
   const  {user, isLoading} = useFetchUserInfoByUserID(comment.postedByID)
+  const setClickedUsersProfile = useProfileInfoStore(state => state.setUserProfileInfo)
 
   const formatDate = (date) => {  //This function just formats the date to either date or time depending when it was posted
                                           //If the comment was done the same year/month/day then we display the time otherwise the date
@@ -22,14 +24,17 @@ const Comment = ({comment}) => {
       return commentDate.toLocaleDateString();
     }
   };
+  const handleNavigateToProfile = () => {
+    setClickedUsersProfile(user)
+  }
 
   return (
     <div>
     {!isLoading ?
       <Flex alignItems="flex-start" mt={2} flexDirection="column" bg={"gray.600"} borderRadius={10} p={"5px"}>
         <Flex  flexDirection={"row"} w={"full"} alignItems={"center"}>
-          <Link to={"/"+user.usernameLower}>
-            <Avatar src={user.profilePicture} name={user.username} size="md" />
+          <Link to={"/"+user.usernameLower} onClick={handleNavigateToProfile} style={{ display: 'block' }}>
+            <Avatar src={user.profilePicture} name={user.username} size="md"/>
           </Link>
             <Flex justifyContent={"space-between"} w={"full"} flexDirection={{md:"row", sm:"column"}} ml={2}>
               <Text fontWeight="bold">{user.username}</Text>
