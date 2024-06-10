6c7bc7d50c0be77408d00bfc8cbbad622687ba86
Fenix900 <Filip.kayar@gmail.com>
2024-06-10 21:52:38 +0200

    We can now search by display name
    We can search by the display name but the search isn't as good as the username since it is capital letter sensitive

--- project-app/src/hooks/useSearchForUser.js
+++ project-app/src/hooks/useSearchForUser.js
@@ -22,20 +22,41 @@ const useSearchForUser = () => {
         }
         try {
           const usersRef = collection(firestore, 'users')
-          const q = query(usersRef, orderBy('usernameLower'), startAt(username.toLowerCase()), endAt(username.toLowerCase() + '\uf8ff')); // Updated query
-          const querySnapshot = await getDocs(q) //all the documents where the "usernameLower" matches the input "username"
-          
-          if (querySnapshot.empty) {
+
+          // Query for usernameLower
+          const usernameQuery = query(
+            usersRef,
+            orderBy('usernameLower'),
+            startAt(username.toLowerCase()),
+            endAt(username.toLowerCase() + '\uf8ff')
+        );
+        // Query for profileDisplayName
+        const profileDisplayNameQuery = query(
+            usersRef,
+            orderBy('profileDisplayName'.toLowerCase()),
+            startAt(username.toLowerCase()),
+            endAt(username.toLowerCase() + '\uf8ff')
+        );
+
+          const querySnapshotUsername = await getDocs(usernameQuery) //all the documents where the "usernameLower" matches the input "username"
+          const querySnapshotDisplayNameQuery = await getDocs(profileDisplayNameQuery) //All documents where we can find the input in the displayName
+          // Combine results, avoiding duplicates
+          const userList = new Map();
+          querySnapshotUsername.forEach((doc) => { //Adds all users with their username
+            userList.set(doc.id, { id: doc.id, ...doc.data() });
+          });
+          querySnapshotDisplayNameQuery.forEach((doc) => { //Adds all users if their display name gave some match
+              if (!userList.has(doc.id)) { //Check if this user is already in the userList
+                  userList.set(doc.id, { id: doc.id, ...doc.data() });
+          }});
+
+          if (userList.size === 0) { //If there are no users found
             setUsers([])
             if(!activeSearch){
               showMessage("No users found","Could not find user with name '"+username+"'", "warning")
             }
           } else {
-            const userList = []; // Collect multiple users
-            querySnapshot.forEach((doc) => {
-                userList.push({ id: doc.id, ...doc.data() });
-            });
-            setUsers(userList); // Set the state with the list of users
+            setUsers(Array.from(userList.values())); // Convert map values to an array
           }
         } catch (e) {
           console.log(e)
